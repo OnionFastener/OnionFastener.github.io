6034a50f015ca0e950ff956d134d0b45f7fa0173
owen song <osong01@students.cs.ubc.ca>
2024-09-02 03:13:50 -0600

    welcome to v1 of my website

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ const highlightMenu = () => {
     const highlighter = document.querySelector('.highlight');
     const homeMenu = document.querySelector('#home-page');
     const aboutMenu = document.querySelector('#about-page');
-    const servicesMenu = document.querySelector('#services-page');
+    const projectsMenu = document.querySelector('#projects-page');
 
     let scrollPos = window.scrollY;
 
@@ -27,18 +27,18 @@ const highlightMenu = () => {
     } else if (window.innerWidth > 960 && scrollPos < 1400) {
         aboutMenu.classList.add('highlight');
         homeMenu.classList.remove('highlight');
-        servicesMenu.classList.remove('highlight');
+        projectsMenu.classList.remove('highlight');
         return;
     } else if (window.innerWidth > 960 && scrollPos < 2345) {
-        servicesMenu.classList.add('highlight');
+        projectsMenu.classList.add('highlight');
         aboutMenu.classList.remove('highlight');
         return;
     }
 
     if(highlighter && window.innerWidth < 960 && scrollPos < 600 || highlighter) {
         highlighter.classList.remove('highlight');
-    }
-}
+    };
+};
 
 window.addEventListener('scroll', highlightMenu);
 window.addEventListener('click', highlightMenu);
@@ -47,10 +47,10 @@ window.addEventListener('click', highlightMenu);
 const closeMobileMenu = () => {
     const menuBars = document.querySelector('.is-active');
     if (window.innerWidth <= 768 && menuBars) {
-        menu.classList.toggle('.is-active');
+        menu.classList.toggle('is-active');
         menuLinks.classList.remove('active');
-    }
-}
+    };
+};
 
 menuLinks.addEventListener('click', closeMobileMenu);
 navLogo.addEventListener('click', closeMobileMenu);
